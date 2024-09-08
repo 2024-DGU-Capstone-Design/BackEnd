9f0d556571862fbbe8c3650457f64b5014245f3b
ksinji <akstp1717@naver.com>
2024-09-09 00:06:00 +0900

    init: 초기 세팅

---
+++ src/main/java/com/example/budd_server/BuddServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.budd_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/budd_server/BuddServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budd_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
