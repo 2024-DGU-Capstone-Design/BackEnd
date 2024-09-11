375d15899888c8db9c8ec2d4558bf970ab25e02c
ksinji <akstp1717@naver.com>
2024-09-12 03:01:45 +0900

    feat: 기능 추가에 따른 컨트롤러 및 서비스 코드 추가

--- src/main/java/com/example/budd_server/controller/ModalController.java
+++ src/main/java/com/example/budd_server/controller/ModalController.java
@@ -1,5 +1,7 @@
 package com.example.budd_server.controller;
 
+import com.example.budd_server.entity.CallRecord;
+import com.example.budd_server.entity.Report;
 import com.example.budd_server.entity.Response;
 import com.example.budd_server.service.ModalService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// 요청할 때
+/* 사용자의 응답 데이터를 조회하려면: /api/call?userId=1&type=response
+   사용자의 레포트 데이터를 조회하려면: /api/call?userId=1&type=report
+   사용자의 통화 기록을 조회하려면: /api/call?userId=1&type=callRecord */
+
 @RestController
 @RequestMapping("/api")
 public class ModalController {
@@ -15,10 +22,27 @@ public class ModalController {
     private ModalService modalService;
 
     @GetMapping("/call")
-            public ResponseEntity<List<Response>> getResponse(@RequestParam int userId){
+    public ResponseEntity<?> getData(@RequestParam int userId, @RequestParam String type) {
+        if ("response".equalsIgnoreCase(type)) {
             List<Response> responses = modalService.getResponse(userId);
-
+            if (responses == null || responses.isEmpty()) {
+                return ResponseEntity.status(404).body("Response 데이터를 찾을 수 없습니다.");
+            }
             return ResponseEntity.ok(responses);
+        } else if ("report".equalsIgnoreCase(type)) {
+            List<Report> reports = modalService.getReportByUserIdAndMonth(userId);
+            if (reports == null || reports.isEmpty()) {
+                return ResponseEntity.status(404).body("Report 데이터를 찾을 수 없습니다.");
+            }
+            return ResponseEntity.ok(reports);
+        } else if ("callRecord".equalsIgnoreCase(type)) {
+            List<CallRecord> callRecords = modalService.getCallRecordByUserIdAndMonth(userId);
+            if (callRecords == null || callRecords.isEmpty()) {
+                return ResponseEntity.status(404).body("callRecords 데이터를 찾을 수 없습니다.");
+            }
+            return ResponseEntity.ok(callRecords);
+        } else {
+            return ResponseEntity.badRequest().body("유효하지 않은 파라미터 입니다.");
+        }
     }
-
 }
