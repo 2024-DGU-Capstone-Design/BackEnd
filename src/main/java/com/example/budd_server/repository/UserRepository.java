5dc6aa4f357d6c1fbaacb8571e5a5aea8c47cda3
SeungJi20 <lina6709@naver.com>
2024-09-29 18:39:52 +0900

    feat: 사용자 목록 페이지네이션 적용, 10명씩 조회되도록 설정

--- src/main/java/com/example/budd_server/repository/UserRepository.java
+++ src/main/java/com/example/budd_server/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.example.budd_server.repository;
 
 import com.example.budd_server.entity.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +15,9 @@ import java.util.Optional;
 public interface UserRepository extends MongoRepository<User, String> {
     List<User> findByName(String name);
     Optional<User> findByUserId(int userId);
+
+    // 페이지네이션
+    Page<User> findAll(Pageable pageable);
 
     @Query(value = "{}", fields = "{ 'userId' : 1 }")
     List<User> findAllUserIds();
