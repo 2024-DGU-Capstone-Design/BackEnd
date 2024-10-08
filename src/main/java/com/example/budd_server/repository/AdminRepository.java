f02772e957876c45dac0d4a850c6817acd3a6c64
Heeseo Kim <chungchae0@gmail.com>
2024-10-08 11:16:05 +0900

    test

--- src/main/java/com/example/budd_server/repository/AdminRepository.java
+++ src/main/java/com/example/budd_server/repository/AdminRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface AdminRepository extends MongoRepository<Admin, String> {
-
+    //test
     Optional<Admin> findByEmail(String email);
 }
