4b85b4b34a1067290a91ea969465433f4bb033b4
ksinji <akstp1717@naver.com>
2024-10-13 23:09:10 +0900

    feat: report 전송에 따른 리포지토리 수정

--- src/main/java/com/example/budd_server/repository/ReportRepository.java
+++ src/main/java/com/example/budd_server/repository/ReportRepository.java
@@ -2,6 +2,7 @@ package com.example.budd_server.repository;
 
 import com.example.budd_server.entity.Report;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,4 +11,7 @@ import java.util.List;
 @Repository
 public interface ReportRepository extends MongoRepository<Report, String> {
     List<Report> findByUserIdAndMonthBetween(Integer userId, LocalDate startMonth, LocalDate endMonth);
+
+    @Query("{ 'userId': ?0, 'month': { $gte: ?1, $lt: ?2 } }")
+    Report findByUserIdAndMonth(int userId, LocalDate startOfMonth, LocalDate startOfNextMonth);
 }
