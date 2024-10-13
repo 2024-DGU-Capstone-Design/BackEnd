b10ec2bd29d27705f73fa2bcabe4a12854f75ff6
ksinji <akstp1717@naver.com>
2024-10-13 23:08:09 +0900

    feat: 응답 상태에 따른 보호자 sms 전송 기능 추가

--- src/main/java/com/example/budd_server/repository/ResponseRepository.java
+++ src/main/java/com/example/budd_server/repository/ResponseRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ResponseRepository extends MongoRepository<Response, String> {
     List<Response> findByUserIdAndDateBetween(int userId, LocalDate startDate, LocalDate endDate);
+
+    List<Response> findByDateBetween(LocalDateTime start, LocalDateTime end);
 }
